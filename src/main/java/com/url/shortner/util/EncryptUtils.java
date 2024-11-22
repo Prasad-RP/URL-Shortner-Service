2bdaf908037b97f24226464615e18d73413cd532
prasad <pansareprasad45@gmail.com>
2024-11-22 15:16:37 +0530

    Random UUID For Unique Hash

--- src/main/java/com/url/shortner/util/EncryptUtils.java
+++ src/main/java/com/url/shortner/util/EncryptUtils.java
@@ -2,14 +2,15 @@ package com.url.shortner.util;
 
 import static com.google.common.hash.Hashing.murmur3_32_fixed;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.UUID.randomUUID;
 
 public class EncryptUtils {
 	private EncryptUtils() {
 	}
-	
+
 	public static String encrypt(String url) {
-		return murmur3_32_fixed().hashString(url, UTF_8).toString();
+		// Adding a unique salt (can use UUID or any random value)
+		return murmur3_32_fixed().hashString(url + randomUUID().toString(), UTF_8).toString();
 	}
-	
-	
+
 }
